02943fe328a13cf8cd4823bb31f3e7b8771b3ec2
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-07-02 10:48:40 +0900

    1. ludium init

--- ft/src/lib.rs
+++ ft/src/lib.rs
@@ -15,6 +15,8 @@ NOTES:
   - To prevent the deployed contract from being modified or deleted, it should not have any access
     keys on its account.
 */
+
+// 가장 먼저 필요한 라이브러리를 가져옵니다.
 use near_contract_standards::fungible_token::metadata::{
     FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC,
 };
@@ -24,17 +26,22 @@ use near_sdk::collections::LazyOption;
 use near_sdk::json_types::U128;
 use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue};
 
+
+// 이 구조체는 fungible token contract의 상태를 정의합니다.
+// 여기에는 토큰 자체와 메타데이터가 포함됩니다.
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
 }
-
+// 토큰의 기본 메타데이터를 정의하는 상수입니다.
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
+// 여기에는 Contract 구조체에 대한 메소드들이 정의되어 있습니다.
 #[near_bindgen]
 impl Contract {
+    // Contract를 기본 메타데이터와 함께 초기화합니다.
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// default metadata (for example purposes only).
     #[init]
@@ -53,7 +60,7 @@ impl Contract {
             },
         )
     }
-
+    // Contract를 주어진 메타데이터로 초기화합니다.
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// the given fungible token metadata.
     #[init]
@@ -78,19 +85,19 @@ impl Contract {
         .emit();
         this
     }
-
+// 계정이 종료될 때 호출되는 콜백입니다.
     fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
         log!("Closed @{} with {}", account_id, balance);
     }
-
+// 토큰이 소각될 때 호출되는 콜백입니다.
     fn on_tokens_burned(&mut self, account_id: AccountId, amount: Balance) {
         log!("Account @{} burned {}", account_id, amount);
     }
 }
-
+// 이 부분은 Fungible Token 스펙의 일부를 구현합니다.
 near_contract_standards::impl_fungible_token_core!(Contract, token, on_tokens_burned);
 near_contract_standards::impl_fungible_token_storage!(Contract, token, on_account_closed);
-
+// 메타데이터를 제공하는 메소드를 구현합니다.
 #[near_bindgen]
 impl FungibleTokenMetadataProvider for Contract {
     fn ft_metadata(&self) -> FungibleTokenMetadata {
